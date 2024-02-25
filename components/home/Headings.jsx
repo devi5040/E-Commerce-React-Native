c1152ff1114a3b84124c8f6b07e8eef97e74346b
devi5040 <dpraidola@gmail.com>
2024-02-25 16:24:32 +0530

    25-02-2024

--- components/home/Headings.jsx
+++ components/home/Headings.jsx
@@ -2,13 +2,16 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import styles from "./headings.style";
 import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../../constants";
 
 const Headings = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>New Rivals</Text>
-        <TouchableOpacity></TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="grid" size={24} color={COLORS.primary} />
+        </TouchableOpacity>
       </View>
     </View>
   );
